5f9ca988267634ee5805189cdddaba5c2dacecff
Juan M. Casillas <juanm.casillas@gmail.com>
2020-03-03 16:08:35 +0100

    Added support for creating coins on the fly and playing with the gravity.

--- ranaVR/Assets/MyScripts/GameStatus.cs
+++ ranaVR/Assets/MyScripts/GameStatus.cs
@@ -7,13 +7,17 @@ public class GameStatus : MonoBehaviour
 {
     public static GameStatus instance = null;
     public int score = 0;
+
+    private bool button_a_pressed = false;
     private TextMeshProUGUI ui_text;
+    private Transform trackingSpace;
 
     private void Awake()
     {
         if (instance == null) { 
             instance = this;
             instance.ui_text = GameObject.Find("score_text").GetComponent<TextMeshProUGUI>();
+            instance.trackingSpace = GameObject.Find("TrackingSpace").GetComponent<Transform>();
         }
         else if (instance != this)
             Destroy(gameObject);
@@ -38,5 +42,53 @@ public class GameStatus : MonoBehaviour
     public static int  GetScore()
     {
         return instance.score;
+    }
+
+
+    public void Update()
+    {
+
+        OVRInput.Update();
+
+        if (OVRInput.Get(OVRInput.Button.One)) {
+
+            instance.button_a_pressed = true;
+            return;
+        }
+        else
+        {
+            if (!instance.button_a_pressed)
+            {
+                return; // button not pressed
+            }
+
+            instance.button_a_pressed = false;
+            //Debug.Log("Button A has been pressed/released");
+            //OVRInput provides touch position and orientation data through GetLocalControllerPosition() and GetLocalControllerRotation(), which return a Vector3 and Quaternion, respectively.
+            // require the tracking space
+            //Vector3 pos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            //Quaternion rot = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+
+
+            Vector3 pos = instance.trackingSpace.TransformPoint(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch));
+            Vector3 rot = instance.trackingSpace.TransformDirection(OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch).eulerAngles);
+       
+            GameObject coin = (GameObject)Instantiate(Resources.Load("coin"));
+            coin.transform.position = pos;
+            coin.transform.rotation = Quaternion.Euler(rot);
+
+            // rotate the coin
+            Quaternion localRotation = Quaternion.Euler(90, 0f, 0f);
+            coin.transform.rotation = coin.transform.rotation * localRotation;
+
+            // toss the coin to upper.
+
+            coin.GetComponent<float_respawn>().start_float();
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        OVRInput.FixedUpdate();
     }
 }
---
+++ ranaVR/Assets/MyScripts/float_respawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class float_respawn : MonoBehaviour
+{
+
+    private float timer = 0.0f;
+    private float max_floating = 0.4f;
+    private bool floating = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (floating) {
+            Rigidbody rigid = GetComponent<Rigidbody>();
+            timer += Time.deltaTime;
+            //float seconds = (timer % 60);            
+            if (timer > max_floating)
+            {
+                  
+                rigid.AddForce(Physics.gravity, ForceMode.Acceleration); // avoid gravity
+                floating = false;
+            }
+            else
+            {
+                
+                rigid.AddForce(-Physics.gravity, ForceMode.Acceleration); // avoid gravity
+            }
+        }
+    }
+
+    public void start_float()
+    {
+        Rigidbody rigid = GetComponent<Rigidbody>();
+        rigid.AddForce(-Physics.gravity * (rigid.mass), ForceMode.Force); // toss up!
+        floating = true;
+    }
+}
